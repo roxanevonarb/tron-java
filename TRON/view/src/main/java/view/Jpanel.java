430172f874bb987969552f00ab35c12b212f2120
xckl <38323288+roxanevonarb@users.noreply.github.com>
2018-07-06 21:28:08 +0200

    Update Jpanel.java

--- TRON/view/src/main/java/view/Jpanel.java
+++ TRON/view/src/main/java/view/Jpanel.java
@@ -1,15 +1,230 @@
-package view;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-import java.awt.*;
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Jpanel extends JPanel {
-	  /**
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	              
-		    
-		  }     
+	private MenuPanel topPanel;
+	private GameLogic logic;
 
+	private JLabel lblScore;
+
+	boolean p1Fire, p2Fire;
+	
+	public static final int W = 1195, H = 855;
+
+
+	
+
+
+	Timer timerGeneral;
+	Timer timerBullet;
+
+	public GamePanel() {
+		
+		
+		setBackground(Color.BLACK);
+		setLayout(new BorderLayout());
+		setSize(W, H);
+	
+		
+		
+		logic = new GameLogic(W, H);
+		lblScore = new JLabel(logic.getP2Name()+": "+logic.getPlayer2Point()+"     "+logic.getP1Name()+": "+logic.getPlayer1Point());
+		lblScore.setForeground(Color.WHITE);
+		lblScore.setHorizontalAlignment(JLabel.CENTER);
+
+		add(lblScore, BorderLayout.SOUTH);
+
+		//TEST!!!!
+		timerGeneral = new Timer(30, this);
+		timerBullet = new Timer(1, this);
+		
+
+	}
+
+	private void p1Fire()
+	{
+
+		logic.fire(logic.getP1());
+		p1Fire = true;
+		timerBullet.start();
+
+	}
+	
+
+	private void p2Fire()
+	{
+		logic.fire(logic.getP2());
+		p2Fire = true;
+		timerBullet.start();
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		if(timerBullet.isRunning())
+			paintBullets(g);
+		logic.draw(g);
+		g.setColor(Color.CYAN);				//bordures
+		g.drawRect(0, 0, W-1, H-1);
+	}
+
+
+	private void paintBullets(Graphics g)
+	{
+		if(p1Fire)
+		{
+			int bulletState = logic.getBullet1().fireStep(W, H);
+			if(bulletState == Bullet.STILL_GO){
+				logic.draw(g);
+				logic.bulletStep(g, bulletState);
+			}
+			else{
+				logic.draw(g);
+				p1Fire = false;
+				timerBullet.stop();
+			}
+			
+		}
+		else if(p2Fire)
+		{
+			int bulletState = logic.getBullet2().fireStep(W, H);
+
+			if(bulletState == Bullet.STILL_GO){   
+				logic.draw(g);
+				logic.bulletStep(g, bulletState);
+			}
+			else{
+				logic.draw(g);
+				p2Fire = false;
+				timerBullet.stop();
+			}
+			
+		}
+		
+	}
+	
+	public void start(){
+		
+		logic.restart();
+		timerGeneral.start();
+		System.out.println(getWidth()+"  "+getHeight());
+	}
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==timerGeneral)
+		{
+			
+			if(logic.stepAndOk())
+				repaint();
+			else
+			{
+				;
+				timerGeneral.stop();	
+				System.out.println("FUCK!");
+				lblScore.setText(logic.getP2Name()+": "+logic.getPlayer2Point()+"     "+logic.getP1Name()+": "+logic.getPlayer1Point());
+			}
+			repaint();
+		}
+		if(e.getSource() == timerBullet)
+		{
+			repaint();
+		}
+
+		
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		/*
+		 * Touches du joueur 1
+		 */
+		if(e.getKeyCode() == KeyEvent.VK_SPACE)
+		{
+			if(logic.getP1().isCanFire()){
+				p1Fire();
+				
+				logic.getP1().setCanFire(false);
+			}
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			if(logic.getP1().getDirection() != Player.UP)
+				logic.setP1Direction(Player.DOWN);
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_UP){
+			if(logic.getP1().getDirection() != Player.DOWN)
+				logic.setP1Direction(Player.UP);
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			if(logic.getP1().getDirection() != Player.RIGHT)
+				logic.setP1Direction(Player.LEFT);
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			if(logic.getP1().getDirection() != Player.LEFT)
+				logic.setP1Direction(Player.RIGHT);
+		}
+
+		/*
+		 * Touches du joueur 2
+		 */
+		if(e.getKeyCode() == KeyEvent.VK_CONTROL)
+		{
+			if(logic.getP2().isCanFire()){
+				p2Fire();
+				
+				logic.getP2().setCanFire(false);
+			}
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_S){
+			if(logic.getP2().getDirection() != Player.UP)
+				logic.setP2Direction(Player.DOWN);
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_Z){
+			if(logic.getP2().getDirection() != Player.DOWN)
+				logic.setP2Direction(Player.UP);
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_Q){
+			if(logic.getP2().getDirection() != Player.RIGHT)
+				logic.setP2Direction(Player.LEFT);
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_D){
+			if(logic.getP2().getDirection() != Player.LEFT)
+				logic.setP2Direction(Player.RIGHT);
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_F2)
+		{
+			start();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}

986c4b20c7e9a3b1d7eeb3d9d58151c71be5fcaf
xckl <38323288+roxanevonarb@users.noreply.github.com>
2018-07-06 21:31:21 +0200

    Update ControllerFacade.java

--- TRON/controller/src/main/java/controller/ControllerFacade.java
+++ TRON/controller/src/main/java/controller/ControllerFacade.java
@@ -1,135 +1,147 @@
-package controller;
+package Game;
 
-import java.sql.SQLException;
-import java.util.List;
-import model.Example;
-import model.IModel;
-import view.IView;
-package controller;
-import java.sql.SQLException;
-import java.util.List;
-import model.Example;
-import model.IModel;
-import view.IView;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player {
+
+	private String name;
+	private int direction, points, intvisibleStepCount;
+	private LengthNode head;
+	private Color color;
+	private boolean canFire;
+	private boolean isVisible, flicker;
 
 
-public class Biker {
+	public static int UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4;
 
-    public enum Direction {
-        NORD,
-        SUD,
-        EST,
-        OUEST
-    }
 
-    private int x = 0;
-    private int y = 0;
+	public static int PIX_PER_STEP = 5, THIKNES = 5;
 
-    private int player;
+	public Player(String name, int direction, Color color, int x, int y, int point)
+	{
+		this.name = name;
+		this.direction = direction;
+		this.color = color;
+		head = new LengthNode(x, y, null);
+		canFire = true;
+		points = point;
+		isVisible = true;
+		flicker = false;
+		intvisibleStepCount = 0;
+	}
 
-    public String name;
 
-    private Direction direction;
 
-    public Biker(int player, String name) {
-        this.player = player;
-        this.name = name;
-        reset();
-    }
+	public boolean isCanFire() {
+		return canFire;
+	}
 
-    public void reset() {
-        if(player == 1) direction = Direction.EST;
-        if(player == 2) direction = Direction.OUEST;
-    }
+	public void setIsVisible(boolean bol)
+	{
+		isVisible = bol;
+	}
 
-    public int getX() {
-        return x;
-    }
+	public void setCanFire(boolean canFire) {
+		this.canFire = canFire;
+	}
 
-    public void setX(int x) {
-        this.x = x;
-    }
+	public boolean getCanFire() {
+		return canFire;
+	}
 
-    public int getY() {
-        return y;
-    }
 
-    public void setY(int y) {
-        this.y = y;
-    }
 
-    public Direction getDirection() {
-        return direction;
-    }
+	public int getPoints() {
+		return points;
+	}
 
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
+	public void addPoints() {
+		this.points++;
+	}
 
-/**
- * <h1>The Class ControllerFacade provides a facade of the Controller component.</h1>
- *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
- */
-public class ControllerFacade implements IController {
+	public String getName() {
+		return name;
+	}
 
-    /** The view. */
-    private final IView  view;
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
 
-    /** The model. */
-    private final IModel model;
 
-    /**
-     * Instantiates a new controller facade.
-     *
-     * @param view
-     *            the view
-     * @param model
-     *            the model
-     */
-    public ControllerFacade(final IView view, final IModel model) {
-        super();
-        this.view = view;
-        this.model = model;
-    }
 
-   
-    
-    
-    
-    
-    public void start() throws SQLException {
-        this.getView().displayMessage(this.getModel().getExampleById(1).toString());
+	public int getDirection() {
+		return direction;
+	}
 
-        this.getView().displayMessage(this.getModel().getExampleByName("Example 2").toString());
 
-        final List<Example> examples = this.getModel().getAllExamples();
-        final StringBuilder message = new StringBuilder();
-        for (final Example example : examples) {
-            message.append(example);
-            message.append('\n');
-        }
-        this.getView().displayMessage(message.toString());
-    }
 
-    /**
-     * Gets the view.
-     *
-     * @return the view
-     */
-    public IView getView() {
-        return this.view;
-    }
+	public LengthNode getHead() {
+		return head;
+	}
 
-    /**
-     * Gets the model.
-     *
-     * @return the model
-     */
-    public IModel getModel() {
-        return this.model;
-    }
+	public void step()
+	{
+		switch(direction)
+		{
+		case 1: 		//haut
+			head = new LengthNode(head.getX(), head.getY() - PIX_PER_STEP, head);
+			break;
+		case 2: 		//bas
+			head = new LengthNode(head.getX(), head.getY() + PIX_PER_STEP, head);
+			break;
+		case 3: 		//gauche
+			head = new LengthNode(head.getX() - PIX_PER_STEP, head.getY(), head);
+			break;
+		case 4: 		//droite
+			head = new LengthNode(head.getX() + PIX_PER_STEP, head.getY(), head);
+			break;
+		}
+
+		if(!isVisible)
+			intvisibleStepCount++;
+		if(intvisibleStepCount == 300)
+			isVisible = true;
+	}
+
+
+
+	public boolean loosed(Player other, int maxWidth, int maxHeight)
+	{
+		//joueur se touche lui-même
+		for(LengthNode p = head.getNext(); p.getNext() != null ; p = p.getNext())
+			if(head.sameValues(p))
+				return true;
+		//il touche un autre joueur
+		for(LengthNode p = other.getHead(); p.getNext() != null ; p = p.getNext())
+			if(head.sameValues(p))
+				return true;
+		//il touche les bordures
+		if(head.getX() < 0 || head.getY() < 0 || head.getX() > maxWidth + THIKNES || head.getY() > maxHeight + THIKNES)
+			return true;
+
+		return false;
+	}
+
+
+	public void draw(Graphics g)
+	{
+		g.setColor(color);
+		LengthNode p = head;
+		if(isVisible)
+			for(;p != null; p=p.getNext())
+				g.fillRect(p.getX(), p.getY(), THIKNES, THIKNES);	//tous les joueurs
+		else 
+			if(flicker){
+				for(;p != null; p=p.getNext())
+					g.fillRect(p.getX(), p.getY(), THIKNES, THIKNES);
+				flicker = false;
+			}else{
+				g.fillRect(p.getX(), p.getY(), THIKNES, THIKNES);
+				int flickerOdds = (int)(Math.random() * 10);
+				if(flickerOdds < 1)
+					flicker = true;
+			}
+	}
+
 }

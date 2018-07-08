463b264c39f5e20ac03f6119bcc5b3c0a6fe9c4c
xckl <38323288+roxanevonarb@users.noreply.github.com>
2018-07-08 12:23:48 +0200

    Update ControllerFacade.java

--- TRON/controller/src/main/java/controller/ControllerFacade.java
+++ TRON/controller/src/main/java/controller/ControllerFacade.java
@@ -1,147 +1,46 @@
-package Game;
+package controller;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import model.ITronModel;
+import view.IView;
 
-public class Player {
-
-	private String name;
-	private int direction, points, intvisibleStepCount;
-	private LengthNode head;
-	private Color color;
-	private boolean canFire;
-	private boolean isVisible, flicker;
-
-
-	public static int UP = 1, DOWN = 2, LEFT = 3, RIGHT = 4;
-
-
-	public static int PIX_PER_STEP = 5, THIKNES = 5;
-
-	public Player(String name, int direction, Color color, int x, int y, int point)
-	{
-		this.name = name;
-		this.direction = direction;
-		this.color = color;
-		head = new LengthNode(x, y, null);
-		canFire = true;
-		points = point;
-		isVisible = true;
-		flicker = false;
-		intvisibleStepCount = 0;
+public class GameControler implements IController {
+	
+	IView view ;
+	ITronModel model;
+	
+	public GameControler(ITronModel model , IView view) {
+		this.model=model;
+		this.view=view;
+		
+		
+		
 	}
-
-
-
-	public boolean isCanFire() {
-		return canFire;
-	}
-
-	public void setIsVisible(boolean bol)
-	{
-		isVisible = bol;
-	}
-
-	public void setCanFire(boolean canFire) {
-		this.canFire = canFire;
-	}
-
-	public boolean getCanFire() {
-		return canFire;
-	}
-
-
-
-	public int getPoints() {
-		return points;
-	}
-
-	public void addPoints() {
-		this.points++;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setDirection(int direction) {
-		this.direction = direction;
-	}
-
-
-
-	public int getDirection() {
-		return direction;
-	}
-
-
-
-	public LengthNode getHead() {
-		return head;
-	}
-
-	public void step()
-	{
-		switch(direction)
-		{
-		case 1: 		//haut
-			head = new LengthNode(head.getX(), head.getY() - PIX_PER_STEP, head);
-			break;
-		case 2: 		//bas
-			head = new LengthNode(head.getX(), head.getY() + PIX_PER_STEP, head);
-			break;
-		case 3: 		//gauche
-			head = new LengthNode(head.getX() - PIX_PER_STEP, head.getY(), head);
-			break;
-		case 4: 		//droite
-			head = new LengthNode(head.getX() + PIX_PER_STEP, head.getY(), head);
-			break;
-		}
-
-		if(!isVisible)
-			intvisibleStepCount++;
-		if(intvisibleStepCount == 300)
-			isVisible = true;
-	}
-
-
-
-	public boolean loosed(Player other, int maxWidth, int maxHeight)
-	{
-		//joueur se touche lui-même
-		for(LengthNode p = head.getNext(); p.getNext() != null ; p = p.getNext())
-			if(head.sameValues(p))
-				return true;
-		//il touche un autre joueur
-		for(LengthNode p = other.getHead(); p.getNext() != null ; p = p.getNext())
-			if(head.sameValues(p))
-				return true;
-		//il touche les bordures
-		if(head.getX() < 0 || head.getY() < 0 || head.getX() > maxWidth + THIKNES || head.getY() > maxHeight + THIKNES)
-			return true;
-
-		return false;
-	}
-
-
-	public void draw(Graphics g)
-	{
-		g.setColor(color);
-		LengthNode p = head;
-		if(isVisible)
-			for(;p != null; p=p.getNext())
-				g.fillRect(p.getX(), p.getY(), THIKNES, THIKNES);	//tous les joueurs
-		else 
-			if(flicker){
-				for(;p != null; p=p.getNext())
-					g.fillRect(p.getX(), p.getY(), THIKNES, THIKNES);
-				flicker = false;
-			}else{
-				g.fillRect(p.getX(), p.getY(), THIKNES, THIKNES);
-				int flickerOdds = (int)(Math.random() * 10);
-				if(flickerOdds < 1)
-					flicker = true;
+	
+	
+	
+	
+	public void play(){
+		
+		while(model.getGrid().getRider1().isAlive() && model.getGrid().getRider2().isAlive()) {
+			model.getGrid().getRider1().move();
+			if(model.getGrid().getRider1().isAlive() == false)
+				this.view.displayMessage("Rider 2 Win (Blue)");
+			
+			
+			
+			
+			model.getGrid().getRider2().move();
+			if(model.getGrid().getRider2().isAlive() == false)
+				this.view.displayMessage("Rider 1 Win (Red) ");
+			try {
+				Thread.sleep(150);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			
+		}
+		
 	}
 
 }
